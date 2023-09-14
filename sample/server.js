94d000af2ba6aa08c29370f039161e4ab214ecb4
Yeung, Yu Ting (David) <ytyeung@users.noreply.github.com>
2023-09-15 00:16:55 +0800

    Update server.js

--- sample/server.js
+++ sample/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end("Hello World!");
+   response.writeHead(200, {'Content-Type': 'text/string'});
+   let dt = new Date();
+   response.end("This is a new string returned on "+ dt.toDateString()+" at "+dt.toLocaleTimeString('en-US'))
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
